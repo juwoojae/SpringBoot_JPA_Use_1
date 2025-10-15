eb386dc41ecb3b0430c1db185bbde2495c4bc02f
Juwoojae <juwqq1234@naver.com>
2025-10-15 09:34:16 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+@AllArgsConstructor
+public class Hello {
+    String name;
+}
